3dab4aa2c8b0d4e6c9f59b59f26b0fbcefe1238c
KudziaAnna <44755987+KudziaAnna@users.noreply.github.com>
2020-10-13 09:34:50 +0200

    Add files via upload

--- SHNet/data_set_SHNet.py
+++ SHNet/data_set_SHNet.py
@@ -7,7 +7,7 @@ Created on Wed Sep 30 09:48:31 2020
 
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
-from SHcoeff import SHcoeff
+from RISH_features import SHBasis
 
 class SHNetDataset(Dataset):
     """Brain dMRI dataset for harmonization"""
@@ -15,10 +15,14 @@ class SHNetDataset(Dataset):
     def __init__(self, b_x, b_y , filename_data, filename_bvals, filename_bvecs):
         'Initialization'
         l_order = 4
-        SH_first = SHcoeff(filename_data, filename_bvals, filename_bvecs, l_order, b_x)
-        SH_second = SHcoeff(filename_data, filename_bvals, filename_bvecs, l_order, b_y)
-        self.input = SH_first.get_SHcoeff()
-        self.label = SH_second.get_SHcoeff()
+        SH_first = SHBasis(filename_data, filename_bvals, filename_bvecs, l_order, b_x)
+        SH_second = SHBasis(filename_data, filename_bvals, filename_bvecs, l_order, b_y)
+        input = SH_first.get_SHCoeff()
+        self.input = np.transpose(input.reshape((input.shape[0], input.shape[1]*input.shape[2]*input.shape[3])))
+        print(self.input.shape)
+        label = SH_second.get_SHCoeff()
+        self.label = np.transpose(label.reshape((label.shape[0], label.shape[1]*label.shape[2]*label.shape[3])))
+    
         if(len(self.input)> len(self.label)):
             self.input = self.input[0: len(self.label)]
         else:
