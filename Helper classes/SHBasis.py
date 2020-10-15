1df1d357efe6820987c105d7884c8e2844f3cbeb
KudziaAnna <44755987+KudziaAnna@users.noreply.github.com>
2020-10-15 15:27:55 +0200

    Add files via upload

--- Helper classes/SHBasis.py
+++ Helper classes/SHBasis.py
@@ -17,6 +17,7 @@ get_SH - public method
 Markings in accordance with  https://onlinelibrary.wiley.com/doi/full/10.1002/mrm.21277
 
 """
+import nilearn
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -28,10 +29,10 @@ from dipy.io import read_bvals_bvecs
 from dipy.reconst.csdeconv import real_sym_sh_basis
 from dipy.core.sphere import cart2sphere
 
+
 class SHBasis:
 
-    
-    def __init__(self, filename_data = '/T1w/Diffusion/data.nii', filename_bvals = '/T1w/Diffusion/bvals', filename_bvecs = '/T1w/Diffusion/bvecs', l_order = 8, bvalue = 3000):
+    def __init__(self, filename_data='122317/T1w/Diffusion/data.nii', filename_bvals='122317/T1w/Diffusion/bvals', filename_bvecs='122317/T1w/Diffusion/bvecs', l_order=4, bvalue=3000):
         img = nib.load(filename_data)
         data_volume = img.get_fdata()
         bvals, bvecs = read_bvals_bvecs(filename_bvals, filename_bvecs)
@@ -44,7 +45,7 @@ class SHBasis:
         
     def _calculate_L(self):
         R = int((self.l_order+1)*(self.l_order+2)/2)
-        L = np.zeros((R,R))
+        L = np.zeros((R, R))
         R_tmp = 1
         l_j = 0
         for j in range(0, R):
@@ -52,37 +53,34 @@ class SHBasis:
                 l_j +=2
                 R_tmp = int((l_j+1)*(l_j+2)/2)
         
-            L[j,j] = np.power(l_j,2)*np.power(l_j+1,2) 
+            L[j, j] = np.power(l_j, 2)*np.power(l_j+1,  2)
         
         return L
-    
-    
+
     def _transformIntoSH(self):
-         r, Phi, Theta = cart2sphere(self.bvecs_sel[:,0], self.bvecs_sel[:,1], self.bvecs_sel[:,2])
-         return r, Phi, Theta
-    
-    
+         _, Phi, Theta = cart2sphere(self.bvecs_sel[:, 0], self.bvecs_sel[:, 1], self.bvecs_sel[:, 2])
+         return Phi, Theta
+
     def _calculate_B(self):
-        r, Phi, Theta = self._transformIntoSH()
+        Phi, Theta = self._transformIntoSH()
         N = self.data.shape[3]
-        for j in range(0,N):
+        for j in range(0, N):
             if j == 0:
                 B = real_sym_sh_basis(self.l_order, Theta[j], Phi[j])[0]
             else:
                 new = real_sym_sh_basis(self.l_order, Theta[j], Phi[j])[0]
-                B  = np.concatenate((B, new))
+                B = np.concatenate((B, new))
         
         return B
-    
-    
+
     def get_SHCoeff(self):
         par_lambda = 0.006
         B = self._calculate_B()
         L = self._calculate_L()
-        S = np.transpose(self.data, (0, 1,3,2))
-        B_s =  np.dot(B.T, S)
-        B_s= np.transpose(B_s,(1,2,0,3))
-        tmp = np.dot(B.T, B)+ par_lambda*L
+        S = np.transpose(self.data, (0, 1, 3, 2))
+        B_s = np.dot(B.T, S)
+        B_s = np.transpose(B_s, (1, 2, 0, 3))
+        tmp = np.dot(B.T, B) + par_lambda*L
         B_l = np.linalg.inv(tmp)   
        
         C = np.dot(B_l, B_s)
@@ -90,42 +88,50 @@ class SHBasis:
     
     def get_RISH(self):
         C = self.get_SHCoeff()
-        size =int( self.l_order/2 +1)
-        RISH = [0]*size
-        for i in range(0,size):
+        size = int(self.l_order/2 + 1)
+        rish = [0]*size
+        for i in range(0, size):
             for j in range(0, 4*i+1):
-                RISH[i] += np.power(C[j], 2)
+                rish[i] += np.power(C[j], 2)
                 
-        return RISH
+        return rish
     
     def get_RISH_from_SHCoeff(self, C):
-        size =int( self.l_order/2 +1)
-        RISH = [0]*size
-        for i in range(0,size):
+        size = int(self.l_order/2 + 1)
+        rish = [0]*size
+        for i in range(0, size):
             for j in range(0, 4*i+1):
-                RISH[i] += np.power(C[j], 2)
+                rish[i] += np.power(C[j], 2)
                 
-        return RISH
+        return rish
     
     def get_SH(self):
-        r, Phi, Theta = self._transformIntoSH()
+        Phi, Theta = self._transformIntoSH()
         SH = real_sym_sh_basis(self.l_order, Theta, Phi)[0]
         return SH
     
     def get_SHdata(self):
-        C = np.transpose(self.get_SHCoeff(), (1,2,0,3))
+        C = np.transpose(self.get_SHCoeff(), (1, 2, 0, 3))
         B = self._calculate_B()
-        return np.transpose(np.dot(B, C), (1,2,3,0))
+        return np.transpose(np.dot(B, C), (1, 2, 3, 0))
+
+    def get_SHdata_from_SHCoeff(self, C):
+        """ C should be R x image size(a,b,c)"""
+        C = np.transpose(C, (1, 2, 0, 3))
+        B = self._calculate_B()
+        print(B.shape)
+        return np.transpose(np.dot(B, C), (1, 2, 3, 0))
     
     def error(self):
         return np.abs(self.data - self.get_SHdata())
-    
+
+
 if __name__ == '__main__':
     subject = '122317'
 
-    #data_dir = '/home/tomasz/data/brain_diffusion/hcp_wuminn/'
+    # data_dir = '/home/tomasz/data/brain_diffusion/hcp_wuminn/'
     data_dir = ''
-    filename_data =  data_dir + subject + '/T1w/Diffusion/data.nii'
+    filename_data = data_dir + subject + '/T1w/Diffusion/data.nii'
     filename_bvals = data_dir + subject + '/T1w/Diffusion/bvals'
     filename_bvecs = data_dir + subject + '/T1w/Diffusion/bvecs'
     
@@ -133,30 +139,28 @@ if __name__ == '__main__':
     RISH = RISH_class.get_RISH()
     SHCoeff = RISH_class.get_SHCoeff()
 
-     
-    plt.imshow(RISH_class.error()[:,:,70, 5], cmap = 'gray' )
+    plt.imshow(RISH_class.error()[:, :, 70, 5], cmap='gray')
     print(np.mean(RISH_class.error()))
-    
-    #%%
+
     s_axial = 70
 
-    plt.subplot(1,5,1)
-    plt.imshow(np.rot90(RISH[0][:,:,s_axial],1), cmap='gray')
+    plt.subplot(1, 5, 1)
+    plt.imshow(np.rot90(RISH[0][:, :, s_axial], 1), cmap='gray')
     plt.axis("off") 
     plt.title("0 order")
-    plt.subplot(1,5,2 )
-    plt.imshow(np.rot90(RISH[1][:,:,s_axial],1), cmap='gray')
+    plt.subplot(1, 5, 2)
+    plt.imshow(np.rot90(RISH[1][:, :, s_axial], 1), cmap='gray')
     plt.axis("off") 
     plt.title("2 order")
-    plt.subplot(1,5,3)
-    plt.imshow(np.rot90(RISH[2][:,:,s_axial],1), cmap='gray') 
+    plt.subplot(1, 5, 3)
+    plt.imshow(np.rot90(RISH[2][:, :, s_axial], 1), cmap='gray')
     plt.axis("off")
     plt.title("4 order") 
-    plt.subplot(1,5,4)
-    plt.imshow(np.rot90(RISH[3][:,:,s_axial],1), cmap='gray')   
+    plt.subplot(1, 5, 4)
+    plt.imshow(np.rot90(RISH[3][:, :, s_axial], 1), cmap='gray')
     plt.axis("off") 
     plt.title("6 order")
-    plt.subplot(1,5,5)
-    plt.imshow(np.rot90(RISH[4][:,:,s_axial],1), cmap='gray')   
+    plt.subplot(1, 5, 5)
+    plt.imshow(np.rot90(RISH[4][:, :, s_axial], 1), cmap='gray')
     plt.axis("off") 
     plt.title("8 order")
